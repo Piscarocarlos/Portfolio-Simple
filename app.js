b034e53f05434f940f04921c4fe7e5225ce98102
Ayi Carlos ALOGNON ANANI <carlosalognon08@gmail.com>
2021-07-29 19:42:33 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+/**
+  Script JS
+  Carlos Alognon
+
+*/
+
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
